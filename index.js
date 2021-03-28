f278c0e80d9daec7be81284175c07a25da966c4e
robert morales <rmoralfit@gmail.com>
2021-03-28 18:59:42 -0400

    commit tree 0a89c799c604b3a5b7000662cb4f90b9e6618083

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+// Code your solution here function 
+function findMatching(collection, string){
+    return collection.filter(match => 
+        match.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(collection, string){
+    return collection.filter(match => 
+        match.toLowerCase().indexOf(string.toLowerCase()) === 0);
+}
+
+function matchName(collection, string){
+    return collection.filter(file => file.name  === string);
+}
